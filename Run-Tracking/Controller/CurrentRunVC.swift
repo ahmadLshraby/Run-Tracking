37e4b112a17c6690cbae2862bacfe6caa676b92b
Ahmad Shraby <ahmadelshraby10@outlook.com>
2019-02-27 09:01:13 +0200

    UI
    CurrentRunVC UI

---
+++ Run-Tracking/Controller/CurrentRunVC.swift
@@ -0,0 +1,22 @@
+//
+//  CurrentRunVC.swift
+//  Run-Tracking
+//
+//  Created by sHiKoOo on 2/27/19.
+//  Copyright © 2019 sHiKoOo. All rights reserved.
+//
+
+import UIKit
+
+class CurrentRunVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+
+}
