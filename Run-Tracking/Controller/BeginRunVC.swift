a7d2f6f10eb571cf4734cafeeeb348e272e75693
Ahmad Shraby <ahmadelshraby10@outlook.com>
2019-02-28 03:55:08 +0200

    LastRunView

--- Run-Tracking/Controller/BeginRunVC.swift
+++ Run-Tracking/Controller/BeginRunVC.swift
@@ -12,6 +12,10 @@ import MapKit
 class BeginRunVC: LocationVC {
 
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var closeBtn: UIButton!
+    @IBOutlet weak var distanceLbl: UILabel!
+    @IBOutlet weak var durationLbl: UILabel!
+    @IBOutlet weak var lastRunView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +29,7 @@ class BeginRunVC: LocationVC {
         super.viewWillAppear(animated)
         locationManager?.delegate = self
         locationManager?.startUpdatingLocation()
+        getLastRun()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -34,6 +39,23 @@ class BeginRunVC: LocationVC {
     
     @IBAction func locationCenterBtn(_ sender: UIButton) {
     }
+    
+    @IBAction func closeBtn(_ sender: UIButton) {
+        lastRunView.isHidden = true
+    }
+    
+    func getLastRun() {
+        if let lastRun = Run.getAllRuns()?.first {
+            distanceLbl.text = "Distance: \(lastRun.distance)"
+            durationLbl.text = "Duration: \(lastRun.duration.formatTimeDurationToString())"
+        }else {
+            lastRunView.isHidden = true
+            return
+            
+        }
+        
+    }
+    
     
     
     
